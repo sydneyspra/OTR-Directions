e896c0c8ebb9f75c02d85d56b3cf210649b32caf
OTR Driver Directions <sydney.spraberry@walmart.com>
2024-08-16 09:58:02 -0400

    Add files via upload

---
+++ otr checkin/1.bash
@@ -0,0 +1,8 @@
+/project-directory
+  /images
+    Picture1.jpg
+    Picture2.jpg
+    Screenshot-2024-07-03-104806.png
+    Screenshot-2024-07-12-092733.png
+    Screenshot-2024-07-12-105458.png
+  test.html
